8ef93d679374e882aaa2f38ad6baefd351f3510e
Bohdan Kravchuk <bohd.kravchuk@gmail.com>
2020-09-29 14:24:03 +0300

    Change results boundary

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -16,13 +16,18 @@ export class MapComponent implements OnInit {
   private apiKey = 'tlWeWjIWwqJYDLAHkVz5B9T5upp7LeHVewaI7Tv23OE';
   private lat = '37.7397';
   private lng = '-121.4252';
-  private radius = '12000';
   private category = 'accommodation';
   private lang = 'en-US';
   private zoom = '14';
   private platform: any;
   private map: any;
   private activeMarker: any;
+  private boundingBox = {
+    aa: -121.43893290993344,
+    la: 37.72537701642574,
+    da: -121.41146708984375,
+    ia: 37.75510599378202
+  };
 
   constructor(private apiService: ApiService) { }
 
@@ -46,17 +51,22 @@ export class MapComponent implements OnInit {
     new H.mapevents.Behavior(new H.mapevents.MapEvents(this.map));
     H.ui.UI.createDefault(this.map, defaultLayers);
     this.map.addEventListener('dragend', this.handleDragEnd);
+    this.map.addEventListener('mapviewchangeend', () => {
+      const newZoom= this.map.getZoom();
+      if (newZoom !== this.zoom) {
+        this.zoom = newZoom;
+        this.handleDragEnd();
+      }
+    })
   }
 
   private handleDragEnd = () => {
-    const { lat, lng } = this.map.getCenter();
-    this.lat = lat;
-    this.lng = lng;
+    this.boundingBox = this.map.getViewModel().getLookAtData().bounds.getBoundingBox();
     this.getHotelsList();
   }
 
   private getHotelsList = () => {
-    const url = `browse?apiKey=${this.apiKey}&in=${this.lat},${this.lng};r=${this.radius}&cat=${this.category}&lang=${this.lang}`;
+    const url = `browse?apiKey=${this.apiKey}&in=${this.createBoundingBoxCoords()}&cat=${this.category}&lang=${this.lang}`;
     this.apiService.getHotels(url)
       .subscribe(
         data => {
@@ -115,6 +125,11 @@ export class MapComponent implements OnInit {
   private scroll = (id) => {
     const el = document.getElementById(id);
     el.scrollIntoView({ behavior: 'smooth', inline: 'center', block: 'center' });
+  }
+
+  private createBoundingBoxCoords = () => {
+    const { aa, la, da, ia } = this.boundingBox;
+    return `${aa},${la},${da},${ia}`;
   }
 
   private createCoords = (place) => ({
